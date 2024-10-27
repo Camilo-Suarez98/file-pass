14cf11663a16b8e021bf307d99b0f33d8030faca
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-10-27 08:25:50 -0500

    change gradient colors

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-[#10001f] flex items-center justify-center">
       {children}
       <Toaster position="top-center" />
     </div>
